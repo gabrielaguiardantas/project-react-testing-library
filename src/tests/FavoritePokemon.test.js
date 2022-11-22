2e6e954dad28ea47e8313e6c9b252f20a11e15c0
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-11-22 19:41:46 -0300

    req até o 4 ok

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+import data from '../data';
+
+describe('some tests in the component FavoritePokemon', () => {
+  it('render "No favorite pokemon found" if doesnt have favorite pokemons at list', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const noFavoritePokemonText = screen.getByText(/no favorite pokémon found/i);
+    expect(noFavoritePokemonText).toBeInTheDocument();
+  });
+  it('render pokemon favorites properly', () => {
+    const pokemonlist = [data[3], data[2]];
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonlist } />);
+    const pokemonName1 = screen.getByText(/ekans/i);
+    const pokemonName2 = screen.getByText(/caterpie/i);
+    expect(pokemonName1).toBeInTheDocument();
+    expect(pokemonName2).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('some tests in the NotFound component', () => {
+  it('contains a h2 with this text: "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const headerNotFoundEl = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    const imgNotFound = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(headerNotFoundEl).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
