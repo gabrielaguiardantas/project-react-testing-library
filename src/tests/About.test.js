faa34ebcd1d6a56d70d17b5bc902ba7c84e4a89e
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-11-22 17:28:22 -0300

    req 1 e 2 ok

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
 
-// bora guyssss vamo que vamooooo
+describe('some tests in component About', () => {
+  it('contains infos about pokedex', () => {
+    renderWithRouter(<About />);
+    const pokedexInfoEl1 = screen.getByText(
+      /this application simulates a pokédex, a digital encyclopedia containing all pokémon/i,
+    );
+    const pokedexInfoEl2 = screen.getByText(
+      /one can filter pokémon by type, and see more details for each one of them/i,
+    );
+    const headingPokedex = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    const imagePokedex = screen.getByRole('img', {
+      name: /pokédex/i,
+    });
+    expect(pokedexInfoEl1).toBeInTheDocument();
+    expect(pokedexInfoEl2).toBeInTheDocument();
+    expect(headingPokedex).toBeInTheDocument();
+    expect(imagePokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('some tests in component App', () => {
+  it('has three links: "Home", "About" and "Favorite Pokemon"', () => {
+    renderWithRouter(<App />);
+    const homeLinkEl = screen.getByRole('link', {
+      name: /home/i,
+    });
+    const aboutLinkEl = screen.getByRole('link', {
+      name: /about/i,
+    });
+    const favoritePokemonLinkEl = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    expect(homeLinkEl).toBeInTheDocument();
+    expect(aboutLinkEl).toBeInTheDocument();
+    expect(favoritePokemonLinkEl).toBeInTheDocument();
+  });
+
+  it('is been redirected to url "/" after click on home link', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLinkEl = screen.getByRole('link', {
+      name: /home/i,
+    });
+    userEvent.click(homeLinkEl);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('is been redirected to url "/about" after click on about link', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLinkEl = screen.getByRole('link', {
+      name: /about/i,
+    });
+    userEvent.click(aboutLinkEl);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('is been redirected to url "/favorites" after click on Favorite Pokémon link', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokemonLinkEl = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    userEvent.click(favoritePokemonLinkEl);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
