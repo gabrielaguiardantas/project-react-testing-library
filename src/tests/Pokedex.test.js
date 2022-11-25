058a1bd447ddab4b3ad45946dddf9da10e51bca0
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-11-25 13:02:44 -0300

    projeto terminado e com algumas lógicas alteradas

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,27 +2,15 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
-import { Pokedex } from '../pages';
-import pokemonList from '../data';
+import App from '../App';
 
 describe('some tests in the component Pokedex', () => {
-  const isPokemonFavoriteById = {
-    4: false,
-    10: false,
-    23: false,
-    25: true,
-    65: false,
-    78: false,
-    143: false,
-    148: false,
-    151: false };
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
   const typeTestId = 'pokemon-type-button';
 
   it('has a h2 with text: "Encountered Pokémon', () => {
-    renderWithRouter(<Pokedex
-      pokemonList={ pokemonList }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
     const headerPokedex = screen.getByRole('heading', {
       name: /encountered pokémon/i,
       level: 2,
@@ -30,55 +18,33 @@ describe('some tests in the component Pokedex', () => {
     expect(headerPokedex).toBeInTheDocument();
   });
   it('shows next pokemon when we click on the "Próximo Pokémon" button', () => {
-    renderWithRouter(<Pokedex
-      pokemonList={ pokemonList }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
     const nextPokemonButton = screen.getByRole('button', {
       name: /próximo pokémon/i,
     });
-    const firstPokemon = screen.getByText(/pikachu/i);
-    expect(nextPokemonButton).toBeInTheDocument();
-    expect(firstPokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    const secondPokemon = screen.getByText(/charmander/i);
-    expect(secondPokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    const thirdPokemon = screen.getByText(/caterpie/i);
-    expect(thirdPokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    userEvent.click(nextPokemonButton);
-    userEvent.click(nextPokemonButton);
-    userEvent.click(nextPokemonButton);
-    userEvent.click(nextPokemonButton);
-    userEvent.click(nextPokemonButton);
-    userEvent.click(nextPokemonButton);
+    const arrayPokemonObj = ['Pikachu', 'Charmander', 'Caterpie'];
+    arrayPokemonObj.forEach((element) => {
+      const pokemon = screen.getByText(element);
+      expect(nextPokemonButton).toBeInTheDocument();
+      expect(pokemon).toBeInTheDocument();
+      userEvent.click(nextPokemonButton);
+    });
+    for (let i = 0; i < 6; i += 1) {
+      userEvent.click(nextPokemonButton);
+    }
     const firstPokemonAgain = screen.getByText(/pikachu/i);
     expect(firstPokemonAgain).toBeInTheDocument();
   });
   it('shows only one pokemon at time on the screen', () => {
-    renderWithRouter(<Pokedex
-      pokemonList={ pokemonList }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
     const pokemonDetailsLink = screen.getAllByRole('link', {
       name: /more details/i,
     });
     expect(pokemonDetailsLink).toHaveLength(1);
   });
   it('has filter buttons on the screen', () => {
-    renderWithRouter(<Pokedex
-      pokemonList={ pokemonList }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
     const filterButtons = screen.getAllByTestId(typeTestId);
     expect(filterButtons).toHaveLength(7);
   });
   it('filter buttons works properly', () => {
-    renderWithRouter(<Pokedex
-      pokemonList={ pokemonList }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
     const filteredButton = screen.getAllByTestId(typeTestId)[0]; // qualquer elemento desse array deve retornar true no expect
     userEvent.click(filteredButton);
     const typeTextOnTheScreen = screen.getAllByText(filteredButton.innerHTML);
@@ -89,10 +55,6 @@ describe('some tests in the component Pokedex', () => {
     expect(allPokemonButton).toBeInTheDocument();
   });
   it('contains a reset filter button', () => {
-    renderWithRouter(<Pokedex
-      pokemonList={ pokemonList }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
     const pikachuNameEl = screen.getByText(/pikachu/i);
     expect(pikachuNameEl).toBeInTheDocument();
     const allPokemonButton = screen.getByRole('button', {
